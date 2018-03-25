c17ed1543712f7ff9796d4e3552cef878616e0b1
mateowinn <mateowinn@gmail.com>
2018-03-24 22:56:02 -0700

    Updated the main app an entry message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<div id="app">
-		<img src="./assets/logo.png">
+		<div class="circle-img-container" style="height: 300px;width: 400px;">
+			<div class="circle-img" style="border-radius: 200px;">
+				<img src="./assets/Blue_Steel.jpg">
+			</div>
+		</div>
 		<router-view/>
 	</div>
 </template>
@@ -19,5 +23,28 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+
+.circle-img-container {
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+}
+
+.circle-img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    position: relative;
+    overflow: hidden;
+}
+
+.circle-img img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
